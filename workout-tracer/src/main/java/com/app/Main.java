78606a46c19f439b41dbf7152e34e91e151d1718
sahin firat <firat.sahin@metu.edu.tr>
2026-05-24 12:18:43 +0300

    Week 3: Finalize GUI and project structure

--- workout-tracer/src/main/java/com/app/Main.java
+++ workout-tracer/src/main/java/com/app/Main.java
@@ -1,16 +1,31 @@
 package com.app;
 
 import com.app.model.Strength;
-import com.app.model.Cardio;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) {
-        // Strength sınıfını test edelim
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Modelden veri al
         Strength s = new Strength("Bench Press", 45, 80);
-        System.out.println(s.getName() + " yakılan kalori: " + s.calculateCalories());
+        String sonuc = s.getName() + " yakılan kalori: " + s.calculateCalories();
 
-        // Cardio sınıfını test edelim
-        Cardio c = new Cardio("Kosu", 30, 5.0);
-        System.out.println(c.getName() + " yakılan kalori: " + c.calculateCalories());
+        // Görsel arayüzü oluştur
+        Label label = new Label(sonuc);
+        VBox root = new VBox(label);
+        Scene scene = new Scene(root, 300, 200);
+
+        primaryStage.setTitle("Fitness Tracker");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
